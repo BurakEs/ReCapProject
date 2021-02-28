54b048d658ac2abc09f6bf1336fed4ddfac3f6ce
BurakEs <burakesen714@gmail.com>
2021-02-28 05:33:43 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,33 @@
+﻿using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarGetAllTest();
+            //CarGetByIdTest(2);
+        }
+
+        private static void CarGetByIdTest(int carId)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            var result = carManager.GetById(carId);
+            Console.WriteLine($"{result.CarId} // {result.Description} // {result.ModelYear} ");
+        }
+
+        private static void CarGetAllTest()
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            var result = carManager.GetAll();
+            foreach (var c in result)
+            {
+                Console.WriteLine($"{c.CarId} // {c.Description} // {c.ModelYear} ");
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal .cs
@@ -0,0 +1,55 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+             new Car{ CarId = 1, BrandId = 1, ColorId = 1, ModelYear = 1985, Description = "Pasat" },
+             new Car {CarId = 2, BrandId = 2, ColorId = 2, ModelYear = 1982, Description = "Opel" }
+             };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = null;
+            carToDelete = _cars.SingleOrDefault(c=>c.CarId==car.CarId);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int carId)
+        {
+            return _cars.FirstOrDefault(c => c.CarId == carId);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = null;
+            carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpdate.CarId = car.CarId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+    }
+}
